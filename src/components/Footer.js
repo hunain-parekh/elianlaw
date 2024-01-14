156f406f525e02370652f0b1b217aaf87a20dd42
Hunain Parekh <hunain.parekh@xloopdigital.com>
2024-01-14 06:19:55 +0500

    Issues resolved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,59 +1,58 @@
 // components/Footer.js
 import React from "react";
 import Link from "next/link";
-import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.mainFooter}>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-3 col-12'>
-              <Link href='/' className={styles.logo}>
+      <div className="mainFooter">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-12">
+              <Link href="/" className="logo">
                 LEXOMLAW
               </Link>
             </div>
-            <div className='col-lg-3 col-md-4 col-12'>
+            <div className="col-lg-3 col-md-4 col-12">
               <ul style={{ textAlign: "left" }}>
                 <li>
-                  <Link href='/'> Home</Link>
+                  <Link href="/"> Home</Link>
                 </li>
                 <li>
-                  <Link href='/about'> About</Link>
+                  <Link href="/about"> About</Link>
                 </li>
                 <li>
-                  <Link href='/contact'> Contact</Link>
+                  <Link href="/contact"> Contact</Link>
                 </li>
               </ul>
             </div>
-            <div className='col-lg-3 col-md-4 col-12'>
+            <div className="col-lg-3 col-md-4 col-12">
               <ul style={{ textAlign: "left" }}>
                 <li>
-                  <Link href='/plan/business'> Business</Link>
+                  <Link href="/plan/business"> Business</Link>
                 </li>
                 <li>
-                  <Link href='/plan/immigration'> Immigration</Link>
+                  <Link href="/plan/immigration"> Immigration</Link>
                 </li>
               </ul>
             </div>
-            <div className='col-lg-3 col-md-4 col-12'>
+            <div className="col-lg-3 col-md-4 col-12">
               <ul style={{ textAlign: "left" }}>
                 <li>
-                  <a href='/'> Facebook</a>
+                  <a href="/"> Facebook</a>
                 </li>
                 <li>
-                  <a href='/'> Instagram</a>
+                  <a href="/"> Instagram</a>
                 </li>
                 <li>
-                  <a href='/'> LinkedIn</a>
+                  <a href="/"> LinkedIn</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className={styles.miniFooter}>Copyright - 2023</div>
       </div>
+      <div className="miniFooter">Copyright - 2023</div>
     </footer>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,122 +1,81 @@
 "use client";
-import React, {useState, useEffect} from "react";
-import styles from "./Navbar.module.css";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/assets/images/logo.png";
 const Navbar = () => {
-  const [checkCount, setCheckCount] = useState(0);
-
-  const googleTranslateElementInit = () => {
-    try {
-      new window.google.translate.TranslateElement(
-        {
-          pageLanguage: "en",
-          autoDisplay: false,
-        },
-        "google_translate_element"
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      try {
-        let checkAlreadyAdded =
-          document.getElementsByClassName("goog-te-combo");
-        console.log(checkAlreadyAdded[0]);
-        if (checkAlreadyAdded[0]) {
-          console.log("Translator already added");
-        } else {
-          setCheckCount(checkCount + 1);
-          var addScript = document.createElement("script");
-          if (checkCount == 0) {
-            addScript.setAttribute(
-              "src",
-              "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-            );
-            document.body.appendChild(addScript);
-            window.googleTranslateElementInit = googleTranslateElementInit;
-          }
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }, 1000);
-  }, []);
-  console.log(checkCount);
-
+  
   return (
-    <nav className={`${styles.navbar} navbar-expand-lg `}>
-      <div className='container'>
-        <Link className={styles.navbarBrand} href='/'>
-          <Image src={Logo} height={80} width={150} alt='logo' />
-        </Link>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNav'
-          aria-controls='navbarNav'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon' />
-        </button>
-        <div className={`collapse ${styles.navbarCollapse}`} id='navbarNav'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <Link className={styles.navLink} aria-current='page' href='/'>
-                Home
-              </Link>
-            </li>
-            <li className={`nav-item dropdown`}>
-              <a
-                className={`${styles.navLink} ${styles.dropdown_toggle}`}
-                href='#'
-                role='button'
-                data-bs-toggle='dropdown'
-                aria-expanded='false'
-              >
-                Plans
-              </a>
-              <ul className={styles.dropdown_menu}>
-                <li>
-                  <Link className='dropdown-item' href='/plan/business'>
-                    Business Law
-                  </Link>
-                </li>
-                <li className={`dropright ${styles.sub_menu}`}>
-                  <Link className='dropdown-item ' href='/plan/immigration'>
-                    Immigration
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className='nav-item'>
-              <Link className={styles.navLink} href='/about'>
-                About Us
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className={styles.navLink} href='/privacy'>
-                Privacy
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <a className={styles.navLink} href='/contact'>
-                Contact
-              </a>
-            </li>
-            <li className='nav-item'>
-              <div id='google_translate_element'></div>
-            </li>
-          </ul>
+    <>
+      <nav className={`navbar navbar-expand-lg `}>
+        <div className="container">
+          <Link className="navbar-brand" href="/">
+            <Image src={Logo} height={80} width={150} alt="logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className={`collapse navbar-collapse`} id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/">
+                  Home
+                </Link>
+              </li>
+              <li className={`nav-item dropdown`}>
+                <a
+                  className={`nav-link dropdown-toggle`}
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Plans
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" href="/plan/business">
+                      Business Law
+                    </Link>
+                  </li>
+                  <li className={`dropright sub-menu`}>
+                    <Link className="dropdown-item " href="/plan/immigration">
+                      Immigration
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="/about">
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="/privacy">
+                  Privacy
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <div id="google_translate_element"></div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
