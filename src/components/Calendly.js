be69f6570cda194cb9809c6cc0acd9d8773e7c06
Hunain Parekh <hunain.parekh@xloopdigital.com>
2024-03-03 03:02:31 +0500

    calendly issue resolved

--- src/components/Calendly.js
+++ src/components/Calendly.js
@@ -1,65 +1,25 @@
-import React, { useEffect } from 'react';
+"use client"
+import React, { useEffect, useState } from "react";
 import { PopupWidget } from "react-calendly";
 
 import { PopupButton } from "react-calendly";
 
-
 const Calendly = () => {
-
-
-
-
-   
-    
-
+  const [rootElement,setRootElement] = useState(null);
+  useEffect(()=>{
+    const body = document.getElementById("root");
+    setRootElement(body);
+  },[setRootElement])
   return (
-
-    
     <>
- 
- 
- {/* <div className="App">
-      <InlineWidget url="https://calendly.com/nomanahmed1919" pageSettings={{
-  backgroundColor: 'ffffff',
-  hideEventTypeDetails: false,
-  hideLandingPageDetails: false,
-  primaryColor: '00a2ff',
-  textColor: '4d5055'
-}}
-styles={{
-    height: '100px'
-  }}
-/>
-    </div> */}
-
-    {/* <PopupWidget
+      <PopupButton
+        className="calendly-color"
         url="https://calendly.com/nomanahmed1919"
-        
-        rootElement={document.getElementById("root")}
         text="Click here to schedule!"
-        textColor="#ffffff"
-        color="black"
-
-        
-      /> */}
-
-
-{/* <a href="#" onClick={(e) => { e.preventDefault(); Calendly.showPopupWidget({ url: 'https://calendly.com/nomanahmed1919' }); }}>Schedule time with me</a> */}
-
-
-
-<PopupButton className='calendly-color'
-        url="https://calendly.com/nomanahmed1919"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
-        rootElement={document.getElementById("root")}
-        text="Click here to schedule!"
+        rootElement={rootElement}
       />
-
     </>
-  )
-}
+  );
+};
 
-export default Calendly
+export default Calendly;
--- src/components/datePickerSection.js
+++ src/components/datePickerSection.js
@@ -1,64 +1,67 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import Link  from 'next/link';
-import StripeCheckout from 'react-stripe-checkout';
-import Calendly from './Calendly';
+import Link from "next/link";
+import StripeCheckout from "react-stripe-checkout";
+import Calendly from "./Calendly";
 
-export const DatePickerSection = ({paymentPagePath}) => {
+export const DatePickerSection = ({ paymentPagePath }) => {
+  const [time, setTime] = useState("");
 
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
 
-
-    const [time,setTime] = useState("")
-
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(null);
-    const onChange = (dates) => {
-      const [start, end] = dates;
-      setStartDate(start);
-      setEndDate(end);
-    };
-
-    console.log(process.env.REACT_APP_STRIPE_PRIVATE_KEY)
-    const onToken = (token) => {
-        console.log(token);
-      }
+  console.log(process.env.REACT_APP_STRIPE_PRIVATE_KEY);
+  const onToken = (token) => {
+    console.log(token);
+  };
   return (
-    <section className='dateSec'>
-        <div className="row">
-            <div className="col-lg-8 col-md-7">
-                <div className="timeMain">
-                    {/* <div className="head">Select Time</div> */}
-                    <div className="timeSelect">
-                    <StripeCheckout 
-                      name="Lexom Law"
-                      token={onToken}
-                      stripeKey={process.env.NEXT_PUBLIC_STRIPE_PRIVATE_KEY}
-                      amount={150 * 100} 
-                      currency="USD">
-
-                        <button className={time == "30 mins" ? "active":""} onClick={()=>setTime("30 mins")}>Pay $150 for 30 Mins</button>
-                        </StripeCheckout>
-                        {/* <button className={time == "1 hour" ? "active":""} onClick={()=>setTime("1 hour")}>1 Hour</button> */}
-                    </div>
-                    <StripeCheckout
-                    
-                    name="Lexom Law"
-                    token={onToken}
-                    stripeKey={process.env.NEXT_PUBLIC_STRIPE_PRIVATE_KEY}
-                    amount={150 * 100} 
-                    currency="USD"
-                    >
-
-                    <Link href={paymentPagePath?paymentPagePath:""} className="makePay">Make Payment To Confirm Your Appointment</Link>
-                    </StripeCheckout>
-
-                </div>
+    <section className="dateSec">
+      <div className="row">
+        <div className="col-lg-8 col-md-7">
+          <div className="timeMain">
+            {/* <div className="head">Select Time</div> */}
+            <div className="timeSelect">
+              <StripeCheckout
+                name="Lexom Law"
+                token={onToken}
+                stripeKey={process.env.NEXT_PUBLIC_STRIPE_PRIVATE_KEY}
+                amount={150 * 100}
+                currency="USD"
+              >
+                <button
+                  className={time == "30 mins" ? "active" : ""}
+                  onClick={() => setTime("30 mins")}
+                >
+                  Pay $150 for 30 Mins
+                </button>
+              </StripeCheckout>
+              {/* <button className={time == "1 hour" ? "active":""} onClick={()=>setTime("1 hour")}>1 Hour</button> */}
             </div>
+            <StripeCheckout
+              name="Lexom Law"
+              token={onToken}
+              stripeKey={process.env.NEXT_PUBLIC_STRIPE_PRIVATE_KEY}
+              amount={150 * 100}
+              currency="USD"
+            >
+              <Link
+                href={paymentPagePath ? paymentPagePath : ""}
+                className="makePay"
+              >
+                Make Payment To Confirm Your Appointment
+              </Link>
+            </StripeCheckout>
+          </div>
+        </div>
 
-
-            <div className="col-lg-4 col-md-5" >
-                {/* <div className="datePicMain">
+        <div className="col-lg-4 col-md-5">
+          {/* <div className="datePicMain">
                     <div className="head">Select Your Date</div>
                     <DatePicker
                     selected={startDate}
@@ -69,17 +72,14 @@ export const DatePickerSection = ({paymentPagePath}) => {
                     inline
                     />
                 </div> */}
-                <div>
-                <h4 className='schapp'>Schedule Appointment</h4>
-
-                </div>
-                <div>
-                <Calendly id='calendly-div'/>
-
-                </div>
-
-            </div>
+          <div>
+            <h4 className="schapp">Schedule Appointment</h4>
+          </div>
+          <div>
+            <Calendly id="calendly-div" />
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
